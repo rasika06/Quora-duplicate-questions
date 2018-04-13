4889d0228521275154e135604bddf08330e7fd28
rasika06 <32562056+rasika06@users.noreply.github.com>
2018-04-13 12:54:56 -0700

    Add files via upload

---
+++ feature_set.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  8 17:38:40 2018
+
+@author: Rasika Telang
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  5 15:00:55 2018
+
+@author: Rasika Telang
+"""
+import pickle as cPickle
+import pandas as pd
+import numpy as np
+import gensim
+from fuzzywuzzy import fuzz
+from nltk.corpus import stopwords
+from tqdm import tqdm
+from scipy.stats import skew, kurtosis
+from scipy.spatial.distance import cosine, cityblock, jaccard, canberra, euclidean, minkowski, braycurtis
+from nltk import word_tokenize
+import gc
+from mpmath import mp
+from sympy import sqrt
+#from mpmath import *
+stop_words = stopwords.words('english')
+
+import os
+os.chdir('C:/Users/Rasika Telang/.spyder-py3/datasets/Quora')
+
+def wmd(s1, s2):
+    s1 = str(s1).lower().split()
+    s2 = str(s2).lower().split()
+    stop_words = stopwords.words('english')
+    s1 = [w for w in s1 if w not in stop_words]
+    s2 = [w for w in s2 if w not in stop_words]
+    return model.wmdistance(s1, s2)
+
+
+def norm_wmd(s1, s2):
+    s1 = str(s1).lower().split()
+    s2 = str(s2).lower().split()
+    stop_words = stopwords.words('english')
+    s1 = [w for w in s1 if w not in stop_words]
+    s2 = [w for w in s2 if w not in stop_words]
+    return model.wmdistance(s1, s2)
+
+
+def sent2vec(s):
+    words = str(s).lower()
+    words = word_tokenize(words)
+    words = [w for w in words if not w in stop_words]
+    words = [w for w in words if w.isalpha()]
+    M = []
+    for w in words:
+        try:
+            M.append(model[w])
+        except:
+            continue
+    M = np.array(M) 
+    v = M.sum(axis=0)
+    return v / sqrt((v ** 2).sum())
+
+gc.collect()
+data = pd.read_csv('train.csv')
+data = data.drop(['id', 'qid1', 'qid2'], axis=1)
+mp.dps = 15
+print("done1")
+'''
+data['len_q1'] = data.question1.apply(lambda x: len(str(x)))
+data['len_q2'] = data.question2.apply(lambda x: len(str(x)))
+data['diff_len'] = data.len_q1 - data.len_q2
+data['len_char_q1'] = data.question1.apply(lambda x: len(''.join(set(str(x).replace(' ', '')))))
+data['len_char_q2'] = data.question2.apply(lambda x: len(''.join(set(str(x).replace(' ', '')))))
+data['len_word_q1'] = data.question1.apply(lambda x: len(str(x).split()))
+data['len_word_q2'] = data.question2.apply(lambda x: len(str(x).split()))
+print("done2")
+gc.collect()
+
+data['common_words'] = data.apply(lambda x: len(set(str(x['question1']).lower().split()).intersection(set(str(x['question2']).lower().split()))), axis=1)
+data['fuzz_qratio'] = data.apply(lambda x: fuzz.QRatio(str(x['question1']), str(x['question2'])), axis=1)
+data['fuzz_WRatio'] = data.apply(lambda x: fuzz.WRatio(str(x['question1']), str(x['question2'])), axis=1)
+data['fuzz_partial_ratio'] = data.apply(lambda x: fuzz.partial_ratio(str(x['question1']), str(x['question2'])), axis=1)
+data['fuzz_partial_token_set_ratio'] = data.apply(lambda x: fuzz.partial_token_set_ratio(str(x['question1']), str(x['question2'])), axis=1)
+data['fuzz_partial_token_sort_ratio'] = data.apply(lambda x: fuzz.partial_token_sort_ratio(str(x['question1']), str(x['question2'])), axis=1)
+data['fuzz_token_set_ratio'] = data.apply(lambda x: fuzz.token_set_ratio(str(x['question1']), str(x['question2'])), axis=1)
+data['fuzz_token_sort_ratio'] = data.apply(lambda x: fuzz.token_sort_ratio(str(x['question1']), str(x['question2'])), axis=1)
+print("done3")
+gc.collect()
+model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin.gz', binary=True)
+data['wmd'] = data.apply(lambda x: wmd(x['question1'], x['question2']), axis=1)
+print("done4")
+gc.collect()
+#norm_model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin.gz', binary=True)
+model.init_sims(replace=True)
+data['norm_wmd'] = data.apply(lambda x: norm_wmd(x['question1'], x['question2']), axis=1)
+print("done5")
+'''
+
+
+model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin.gz', binary=True)
+question1_vectors = np.zeros((data.shape[0], 300))
+error_count = 0
+
+for i, q in tqdm(enumerate(data.question1.values)):
+    question1_vectors[i, 100000] = sent2vec(q)
+print("done6")
+question2_vectors  = np.zeros((data.shape[0], 300))
+for i, q in tqdm(enumerate(data.question2.values)):
+    question2_vectors[i, :] = sent2vec(q)
+print("done7")
+
+data['cosine_distance'] = [cosine(x, y) for (x, y) in zip(np.nan_to_num(question1_vectors),
+                                                          np.nan_to_num(question2_vectors))]
+print("done8")
+data['cityblock_distance'] = [cityblock(x, y) for (x, y) in zip(np.nan_to_num(question1_vectors),
+                                                          np.nan_to_num(question2_vectors))]
+
+data['jaccard_distance'] = [jaccard(x, y) for (x, y) in zip(np.nan_to_num(question1_vectors),
+                                                          np.nan_to_num(question2_vectors))]
+
+data['canberra_distance'] = [canberra(x, y) for (x, y) in zip(np.nan_to_num(question1_vectors),
+                                                          np.nan_to_num(question2_vectors))]
+print("done9")
+data['euclidean_distance'] = [euclidean(x, y) for (x, y) in zip(np.nan_to_num(question1_vectors),
+                                                          np.nan_to_num(question2_vectors))]
+
+data['minkowski_distance'] = [minkowski(x, y, 3) for (x, y) in zip(np.nan_to_num(question1_vectors),
+                                                          np.nan_to_num(question2_vectors))]
+
+data['braycurtis_distance'] = [braycurtis(x, y) for (x, y) in zip(np.nan_to_num(question1_vectors),
+                                                          np.nan_to_num(question2_vectors))]
+
+data['skew_q1vec'] = [skew(x) for x in np.nan_to_num(question1_vectors)]
+data['skew_q2vec'] = [skew(x) for x in np.nan_to_num(question2_vectors)]
+data['kur_q1vec'] = [kurtosis(x) for x in np.nan_to_num(question1_vectors)]
+data['kur_q2vec'] = [kurtosis(x) for x in np.nan_to_num(question2_vectors)]
+print("done10")
+cPickle.dump(question1_vectors, open('data/q1_w2v.pkl', 'wb'), -1)
+cPickle.dump(question2_vectors, open('data/q2_w2v.pkl', 'wb'), -1)
+
+data.to_csv('data/quora_features.csv', index=False)
+print("done all")
